2b88be57bb44825915163a4aaedc35185fb2c7d2
csgorham <caroling@alumni.cmu.edu>
2024-04-11 20:00:33 -0600

    openapi.json

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, jsonify
-from flask_openapi3 import Api
-app = Flask(__name__)
-api = Api(app)
+
+from flask import Flask, request, jsonify
+from flask_openapi3 import OpenAPI, Info, Tag
+#app = Flask(__name__)
+#extension = FlaskOpenAPIExtension(app)
+
+info = Info(title="Developer Pricing API", version="1.0.0")
+app = OpenAPI(__name__, info=info)
 
 
 @app.route("/")
 def hello_world():
     return jsonify({"message": "Hello World!"})
 
-spec = api.spec
-with open('openapi.json', 'w') as f:
-    json.dump(spec, f)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    #app.run(debug=True)
