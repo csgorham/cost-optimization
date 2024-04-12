aa361c23d91268af155e584a5776e809a9c7f37d
csgorham <caroling@alumni.cmu.edu>
2024-04-12 12:15:47 -0600

    1

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, request, jsonify
+from pydantic import BaseModel
 
-from flask import Flask, request, jsonify
+#from flask import Flask, request, jsonify
 from flask_openapi3 import OpenAPI, Info, Tag
+#from flask_rest_api import Api
 #app = Flask(__name__)
 #extension = FlaskOpenAPIExtension(app)
 
 info = Info(title="Developer Pricing API", version="1.0.0")
 app = OpenAPI(__name__, info=info)
+#api = Api(app)
+
+class DeveloperPricing(BaseModel):
+    db: str
+    ml: str
 
 
 @app.route("/")
@@ -15,5 +21,5 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    #app.run(debug=True)
+    #app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
