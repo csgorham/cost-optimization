630168d48acbe78d3bd1cb39774d3d16bb7a7a31
csgorham <caroling@alumni.cmu.edu>
2024-04-12 18:38:18 -0600

    1

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel
 
-from flask import Flask, request, jsonify
 #app = Flask(__name__)
-
-
 #from flask_openapi3 import OpenAPI, Info, Tag
 #info = Info(title="Developer Pricing API", version="1.0.0")
 #app = OpenAPI(__name__, info=info)
 
+from flask import Flask, request, jsonify
 from apiflask import APIFlask
 app = APIFlask(__name__, spec_path='/openapi.json')
 app.config['OPENAPI_VERSION'] = '3.0.2'
@@ -17,9 +15,11 @@ class DeveloperPricing(BaseModel):
     db: str
     ml: str
 
-@app.route("/")
-def hello_world():
-    return jsonify({"message": "Hello World!"})
+@app.route("/calc_pricing_options', methods=["POST", "GET"])
+def calc_pricing_options():
+    products = request.json.get('products')
+    options = "Options " + products
+    return  options #jsonify({"message": "Hello World!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
