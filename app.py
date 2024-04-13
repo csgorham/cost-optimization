fe4b5b167354532a8c033c92e946642c9322bb42
csgorham <caroling@alumni.cmu.edu>
2024-04-12 20:08:26 -0600

    1

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ from pydantic import BaseModel
 #info = Info(title="Developer Pricing API", version="1.0.0")
 #app = OpenAPI(__name__, info=info)
 from flask_restful import reqparse
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from apiflask import APIFlask
-from os
+import os
 
 app = APIFlask(__name__, spec_path='/openapi.json')
 app.config['OPENAPI_VERSION'] = '3.0.2'
-
 app.config['SYNC_LOCAL_SPEC'] = True
 app.config['LOCAL_SPEC_PATH'] = os.path.join(app.root_path, 'openapi.json')
+
+users = {
+    'kunal': '1234',
+    'user2': 'password2'
+}
 
 products = {"database": 5 , "cloud" : 2}
 languages = languages = [
@@ -22,18 +26,38 @@ languages = languages = [
 
 @app.route("/")
 def home():
-    return jsonify({
-            "status": "online"
-        })
+    return render_template('login.html')
 
-@app.route("/get_product", methods=["GET"])
-def get_product():
+@app.route('/handle_get', methods=['GET'])
+def handle_get():
+    if request.method == 'GET':
+        username = request.args['username']
+        password = request.args['password']
+        print(username, password)
+        if username in users and users[username] == password:
+            return '<h1>Welcome!!!</h1>'
+        else:
+            return '<h1>invalid credentials!</h1>'
+    else:
+        return render_template('login.html')
 
-    #product = request.json.get('message')
-    #print(product)
-    return jsonify({
-        "languages": languages
-    })
+
+# For handling post request form we can get the form
+# inputs value by using POST attribute.
+# this values after submitting you will never see in the urls.
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if username in users and users[username] == password:
+            return '<h1>Welcome!!!</h1>'
+        else:
+            return '<h1>invalid credentials!</h1>'
+    else:
+        return render_template('login.html')
+
 
 def parse_arg_from_requests(arg, **kwargs):
     parse = reqparse.RequestParser()
